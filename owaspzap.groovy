1fd31005458877f3f09d2088474380a01bba955c
surbano <36646879+surbano@users.noreply.github.com>
2020-02-10 10:52:55 -0500

    Update owaspzap.groovy

--- owaspzap.groovy
+++ owaspzap.groovy
@@ -1,18 +1,17 @@
 node { 
-	stage 'Checkout'
-        checkout scm
+	agent none
 	
 	//echo 'mariko'
     //stages {    	  
 	 
-      //stage ('Owasp ZAP Analysis'){
+      stage ('Owasp ZAP Analysis'){
 	      //steps {		
-		//echo 'mariko'
+		echo 'mariko'
           // Se ejecuta la tarea "prueba-demo1" del Jenkins de manera externa la cual contiene el owaszap configurado 
           //build job:  '/prueba-demo1', parameters: [string(name: 'param1', value:'val1')], wait: true   
 
         //}
 	 
-      //}
+      }
     //}
   }
